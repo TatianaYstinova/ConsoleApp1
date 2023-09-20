615a09cd1145300c8c93dccd495f823f9a9042c5
ustin <rykova.tanya.96@gmail.com>
2023-09-20 22:13:51 +0300

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Sort
+    {
+        public static  int[] Sortirofka (int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int maxIndex = i;
+
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[j] > array[maxIndex])
+                    {
+                        maxIndex = j;
+                    }
+                }
+                int tmp = array[maxIndex];
+                array[maxIndex] = array[i];
+                array[i] = tmp;
+            }
+            return array;
+        }
+        public static void Write ( int[] array)
+{
+    for( int i = 0; i < array.Length ; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+}
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿using ConsoleApp1;
+int[] a = new int[] { 2,4,9,2,3,5,10};
+a=Sort.Sortirofka(a);
+Console.Write(a);
+
+
